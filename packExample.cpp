98e0b9da8516125fab9bbd70abfc09cfa57ec4db
Federico Liuzzi <57435671+fliuzzi02@users.noreply.github.com>
2026-03-02 11:11:53 +0000

    Add more examples

---
+++ packExample.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <nmealib/nmea0183/rmc.hpp>
+#include <nmealib/nmea0183/gga.hpp>
+
+int main() {
+    try {
+        nmealib::nmea0183::RMC rmc_msg(
+            "GP", 
+            123519, 
+            'A', 
+            4807.038, 
+            'N', 
+            1131.000, 
+            'E', 
+            22.4, 
+            84.4, 
+            230394, 
+            3.1, 
+            'W', 
+            'A', 
+            'V'
+        );
+        std::cout << "Packed RMC: " << rmc_msg.serialize() << "\n";
+
+        nmealib::nmea0183::GGA gga_msg("GP", 123519, 4807.038, 'N', 1131.000, 'E', 1,8, 0.9, 200.0, 'M', 46.9, 'M', 0.0, "");
+        std::cout << "Packed GGA: " << gga_msg.serialize() << "\n";
+    } catch (const nmealib::NmeaException& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+
+    return 0;
+}
